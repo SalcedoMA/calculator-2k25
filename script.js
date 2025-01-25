1363dd5d82c9bb21745488ec9676b439f44f2768
salcedoma <salcedoma.github@gmail.com>
2025-01-25 14:38:44 -0300

    Get button values and store screen results
    define variables to store necesary values

--- script.js
+++ script.js
@@ -30,3 +30,80 @@ function operate(value1, value2, operator) {
             break;
     }
 }
+
+
+const display = document.querySelector('.input');
+const equal = document.querySelector('#equal');
+const clear = document.querySelector('#clear')
+const numButtons = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+
+let previousValue;
+    let lastValue;
+    let operatorOnFocus;
+    let lastOperator;
+    let count;
+    let opClicked;
+
+function reset() {
+    previousValue = 0;
+    lastValue = 0;
+    operatorOnFocus = false;
+    lastOperator;
+    count = 0;
+}
+
+reset();
+
+function saveInputs() {
+    previousValue = lastValue;
+    lastValue = parseFloat(display.innerText);
+}
+ 
+
+equal.addEventListener('click', event => {
+
+})
+
+clear.addEventListener('click', event => {
+    reset();
+})
+
+// function getValue(event) {
+//     if (event.target.matches('button')) {
+//         return event.target.value;
+//     }
+// }
+
+//GET BUTTON VALUES
+//NUMBER EVENTS
+for (let number of numButtons) {
+    let btnClicked;
+    number.addEventListener('click', event => {
+        if (event.target.matches('button')) {
+            btnClicked = event.target.value;
+        }
+        if (operatorOnFocus) {
+            display.innerText = "";
+            operatorOnFocus = false;
+        }
+        display.innerText += btnClicked;
+
+    })
+    
+}
+//OPERATOR EVENTS
+for (let operator of operators) {
+    operator.addEventListener('click', event => {
+        if (event.target.matches('button')) {
+            let operatorClicked = event.target.value;
+        }
+
+        if (!operatorOnFocus) {
+            saveInputs();
+        }
+        
+        operatorOnFocus = true;
+        console.log(previousValue, lastValue);
+    })
+}  
